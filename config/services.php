5fff26792165c8598ba6a69d9a2f28d0f3b9996a
Jamming0629 <ttajitaji1234taji@gmail.com>
2021-09-27 17:56:43 +0900

    fix1

--- config/services.php
+++ config/services.php
@@ -37,6 +37,6 @@ return [
     'google' => [
     'client_id' => env('GOOGLE_CLIENT_ID'),
     'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-    'redirect' => "https://vast-brushlands-90603.herokuapp.com/login/google/callback",
+    'redirect' => env('GOOGLE_URL'),
     ]
 ];
